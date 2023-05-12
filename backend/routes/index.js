118fa2228fba0ccde25a6338cc02f4df497cf20a
Vasiliy <thewargas@mail.ru>
2023-05-12 16:00:04 +0300

    fix: routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,14 +10,14 @@ const {
 } = require('../controllers/users');
 const { urlRegExp } = require('../utils/constants');
 
-router.post('/sign-in', celebrate({
+router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 
-router.post('/sign-up', celebrate({
+router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
