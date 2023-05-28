2cf775e1e8a26fbf7cbe36c17d40bfa45588ba08
Vasiliy Lavrov <71934240+thewargas@users.noreply.github.com>
2023-05-28 11:43:38 +0300

    fix port

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const selectErrors = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const enableCors = require('./middlewares/cors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
